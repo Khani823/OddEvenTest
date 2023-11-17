e5f0049595cdd22de7deee449dea325882a66a5e
Khani823 <149378495+Khani823@users.noreply.github.com>
2023-11-17 10:22:29 +0900

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp11
+{
+    internal class Solution
+    {
+        static public string solution(int num)
+        {
+            return (num % 2 == 0) ? "Even" : "Odd";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("정수를 입력하세요: ");
+            int num;
+
+            // 사용자로부터 정수를 입력받습니다.
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                string result = solution(num);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("유효한 정수를 입력하세요.");
+            }
+
+        }
+    }
+}
